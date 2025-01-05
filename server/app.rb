20b9dfb4703f40be3547e0fa9d4a7c355aa866b0
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2025-01-04 19:30:46 -0600

    Add support to broadcast to every client at the time

--- server/app.rb
+++ server/app.rb
@@ -1,22 +1,41 @@
 # frozen_string_literal: true
 
 require 'faye/websocket'
+require 'json'
 
 App = lambda do |env|
+  @clients ||= []
+
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env)
 
     ws.on :open do |_event|
       puts 'Client connected'
+      @clients << ws
     end
 
     ws.on :message do |event|
       puts "Received message: #{event.data}"
-      ws.send(event.data)
+
+      @clients.each do |client|
+        next if client == ws
+
+        client.send(event.data)
+      end
     end
 
     ws.on :close do |event|
       p [:close, event.code, event.reason]
+
+      @clients.each do |client|
+        next if client == ws
+
+        data = { name: 'System', message: 'Someone has disconnected' }
+
+        client.send data.to_json
+      end
+
+      @clients.delete ws
       ws = nil
     end
 
